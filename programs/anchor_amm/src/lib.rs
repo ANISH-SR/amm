7bb22c8291413f396a76ddd66843776beba654ef
ANISH-SR <rawatanish08@gmail.com>
2025-07-22 12:50:53 +0000

    [init]

---
+++ programs/anchor_amm/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("79AUw6xa8AivPPeK4aGYoPr1RaqLspgASSgW6SawNDwt");
+
+#[program]
+pub mod anchor_amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
